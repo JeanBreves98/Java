0345d88909a7268d82898a0afe4d54120ceec1c4
Jean <JeanBreves1998@gmail.com>
2025-08-27 20:31:32 -0300

    27/08/2025 - 20:31

--- Learning/TreeSetChallenge/src/dev/lpa/BookingAgent.java
+++ Learning/TreeSetChallenge/src/dev/lpa/BookingAgent.java
@@ -18,6 +18,14 @@ public class BookingAgent {
         bookSeat(rodgersNYC, 'B', 11);
         bookSeat(rodgersNYC, 'M', 1);
 
+        bookSeats(rodgersNYC, 4, 'B', 3, 10);
+        bookSeats(rodgersNYC, 6, 'B', 'C', 3, 10);
+        bookSeats(rodgersNYC, 4, 'B', 1, 10);
+        bookSeats(rodgersNYC, 4, 'B', 'C', 1, 10);
+        bookSeats(rodgersNYC, 1, 'B', 'C', 1, 10);
+        bookSeats(rodgersNYC, 4, 'M', 'Z', 1, 10);
+        bookSeats(rodgersNYC, 10, 'A', 'E', 1, 10);
+
     }
 
     private static void bookSeat(Theater theater, char row, int seatNo) {
@@ -28,6 +36,20 @@ public class BookingAgent {
             theater.printSeatMap();
         } else {
             System.out.println("Sorry! Unable to reserve " + row + seatNo);
+        }
+    }
+
+    private static void bookSeats(Theater theater, int tickets, char minRow, int minSeat, int maxSeat) {
+        bookSeats(theater, tickets, minRow, minRow, minSeat, maxSeat);
+    }
+    private static void bookSeats(Theater theater, int tickets, char minRow, char maxRow, int minSeat, int maxSeat) {
+
+        var seats = theater.reserveSeats(tickets, minRow, maxRow, minSeat, maxSeat);
+        if (seats != null) {
+            System.out.println("Congratulations! Your reserved seats are " + seats);
+            theater.printSeatMap();
+        } else {
+            System.out.println("Sorry! No matching contiguous seats in rows: " + minRow + " - " + maxRow);
         }
     }
 }
