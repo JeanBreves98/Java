5047420ede63d55fa48869bf61fe1607782d4c37
Jean <JeanBreves1998@gmail.com>
2025-09-09 17:51:59 -0300

    09/09/2025 - 17:51

--- Learning/GameConsole/src/dev/lpa/Main.java
+++ Learning/GameConsole/src/dev/lpa/Main.java
@@ -1,7 +1,6 @@
 package dev.lpa;
 
 import dev.lpa.game.GameConsole;
-import dev.lpa.game.ShooterGame;
 import dev.lpa.pirate.Pirate;
 import dev.lpa.pirate.PirateGame;
 import dev.lpa.pirate.Weapon;
@@ -13,19 +12,25 @@ public class Main {
 //        int playerIndex = console.addPlayer();
 //        console.playGame(playerIndex);
 
-        Weapon weapon = Weapon.getWeaponByChar('P');
-        System.out.println("weapon = " + weapon + ", hitPoints=" +
-                weapon.getHitPoints() + ", minLevel=" + weapon.getMinLevel());
+//        Weapon weapon = Weapon.getWeaponByChar('P');
+//        System.out.println("weapon = " + weapon + ", hitPoints=" +
+//                weapon.getHitPoints() + ", minLevel=" + weapon.getMinLevel());
+//
+//        var list = Weapon.getWeaponsByLevel(1);
+//        list.forEach(System.out::println);
+//
+//        Pirate tim = new Pirate("Tim");
+//        System.out.println(tim);
+//
+//        PirateGame.getTowns(0).forEach(t -> System.out.println(t.information()));
+//        System.out.println("-".repeat(100));
+//        PirateGame.getTowns(1).forEach(t -> System.out.println(t.information()));
 
-        var list = Weapon.getWeaponsByLevel(1);
-        list.forEach(System.out::println);
+//        Town bridgetown = new Town("Bridgetown", "Barbados", 0);
+//        System.out.println(bridgetown);
+//        System.out.println(bridgetown.information());
 
-        Pirate tim = new Pirate("Tim");
-        System.out.println(tim);
 
-        PirateGame.getTowns(0).forEach(System.out::println);
-        System.out.println("-".repeat(100));
-        PirateGame.getTowns(1).forEach(System.out::println);
 
         var console = new GameConsole<>(new PirateGame("The Pirate Game"));
         int playerIndex = console.addPlayer();
