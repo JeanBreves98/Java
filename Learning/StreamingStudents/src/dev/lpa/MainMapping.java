d967499e77644575c47c3ad2faa4c397fb9cfb4c
Jean <JeanBreves1998@gmail.com>
2025-09-30 18:46:34 -0300

    30/09/2025 - 18:46

--- Learning/StreamingStudents/src/dev/lpa/MainMapping.java
+++ Learning/StreamingStudents/src/dev/lpa/MainMapping.java
@@ -55,5 +55,46 @@ public class MainMapping {
             value.forEach((key1, value1) ->
             System.out.println("\t" + key1 + " " + value1.size()));
         });
+
+//        long studentCount = students.stream().count();
+        long studentBodyCount = 0;
+        for (var list : experienced.values()) {
+            studentBodyCount += list.size();
+        }
+        System.out.println("studentBodyCount = " + studentBodyCount);
+
+        studentBodyCount = experienced.values().stream().mapToInt(l -> l.size()).sum();
+        System.out.println("studentBodyCount = " + studentBodyCount);
+
+        studentBodyCount = experienced.values().stream()
+                .map(l -> l.stream()
+                        .filter(s -> s.getMonthsSinceActive() <= 3)
+                        .count()
+                )
+                        .mapToLong(l -> l)
+                        .sum();
+        System.out.println("studentBodyCount = " + studentBodyCount);
+
+        long count = experienced.values().stream()
+                .flatMap(l -> l.stream())
+                .filter(s -> s.getMonthsSinceActive() <= 3)
+                .count();
+        System.out.println("Active students = " + count);
+
+        count = multilevel.values().stream()
+                .flatMap(map -> map.values().stream()
+                        .flatMap(l -> l.stream())
+                )
+                .filter(s -> s.getMonthsSinceActive() <= 3)
+                .count();
+        System.out.println("Active students in multilevel = " + count);
+
+        count = multilevel.values().stream()
+                .flatMap(map -> map.values().stream())
+                .flatMap(l -> l.stream())
+                .filter(s -> s.getMonthsSinceActive() <= 3)
+                .count();
+
+        System.out.println("Active students in multilevel = " + count);
     }
 }
