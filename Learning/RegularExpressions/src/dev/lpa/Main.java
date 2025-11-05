f632c38963dbfeb7c4e87db6d8f11774267f3cc5
Jean <JeanBreves1998@gmail.com>
2025-11-05 15:55:14 -0300

    05/11/2025 - 15:55

--- Learning/RegularExpressions/src/dev/lpa/Main.java
+++ Learning/RegularExpressions/src/dev/lpa/Main.java
@@ -11,6 +11,36 @@ public class Main {
 
         String helloWorld3 = Main.format("%s %s", "Hello", "World");
         System.out.println("Using Main.format: " + helloWorld3);
+
+        String testString = "Anyone can Learn abc's, 123's, and any regular expression";
+        String replacement = "(-)";
+
+        String[] patterns = {
+                "abc",
+                "123",
+                "A",
+                "[abc]",
+                "[123]",
+                "[A]",
+                "a|b|c",
+                "[a-z]",
+                "[0-9]",
+                "[A-Z]",
+                "[a-zA-Z]",
+                "[a-zA-Z]*",
+                "[0-9]*",
+                "[A-Z]*",
+                "[0-9]+",
+                "[0-9]{2}+",
+                "[a-zA-Z]*$",
+                "^[a-zA-Z]{3}",
+                "[aA]ny\\b",
+        };
+
+        for (String pattern : patterns) {
+            String output = testString.replaceFirst(pattern, replacement);
+            System.out.println("Pattern: " + pattern + " => " + output);
+        }
     }
 
     private static String format(String regexp, String... args) {
